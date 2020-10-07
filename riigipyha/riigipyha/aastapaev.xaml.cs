aada776137f8b920d813572027362d1791d27133
Opilane TTHK <opilane@tthk.ee>
2020-10-07 12:30:28 +0300

    TabbedPage

---
+++ riigipyha/riigipyha/aastapaev.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace riigipyha
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class aastapaev : ContentPage
+    {
+        public aastapaev()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ riigipyha/riigipyha/kevad.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace riigipyha
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class kevad : ContentPage
+    {
+        public kevad()
+        {
+            InitializeComponent();
+        }
+    }
+}
